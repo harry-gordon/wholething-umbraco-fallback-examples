6faf08287cca54823c39c31c5f071ec9f0140f4b
Harry Gordon <harrygordon1000@gmail.com>
2021-11-17 15:29:23 +0000

    Installed fallback and added example

---
+++ Wholething.Umbraco.Examples.Umbraco8/App_Plugins/FallbackTextstring/fallback-text-service.js
@@ -0,0 +1,16 @@
+﻿var umbraco = angular.module('umbraco');
+
+umbraco.factory('fallbackTextService', ['$http', function ($http) {
+    var baseUrl = '/Umbraco/FallbackText';
+
+    function getTemplateData(nodeId, propertyAlias) {
+        return $http.get(`${baseUrl}/TemplateData/Get?nodeId=${nodeId}&propertyAlias=${propertyAlias}`).then(function (data) {
+            return data.data;
+        });
+    };
+
+    var service = {
+        getTemplateData: getTemplateData
+    };
+    return service;
+}]);
